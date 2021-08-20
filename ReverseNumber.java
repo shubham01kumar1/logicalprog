6f25135c6b230f681026f400f33640470c1cb452
shubham01kumar1 <shubham01kumar1@gmail.com>
2021-08-21 00:09:18 +0530

    program to check for Reverse Number

--- ReverseNumber.java
+++ ReverseNumber.java
@@ -1,2 +1,22 @@
-package logicalprog;public class ReverseNumber {
+package logicalprog;
+import java.util.Scanner;
+
+public class ReverseNumber {
+    public static void main(String[] args) {
+
+        System.out.println("Enter the number to reverse:");
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        int revNum = reverse(num);
+        System.out.println("reverse number is:" + revNum);
+    }
+    public static int reverse(int num) {
+        int rev = 0;
+        while (num > 0) {
+            int remainder = num % 10;
+            num /= 10;
+            rev = rev * 10 + remainder;
+        }
+        return rev;
+    }
 }
