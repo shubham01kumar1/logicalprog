64286eea79580ea3bcde0ff2d4c38a1ec87ce10a
shubham01kumar1 <shubham01kumar1@gmail.com>
2021-08-21 00:10:04 +0530

    program to simulate a stop watch

--- StopWatch.java
+++ StopWatch.java
@@ -1,2 +1,16 @@
-package logicalprog;public class StopWatch {
+package logicalprog;
+import java.util.Scanner;
+
+public class StopWatch {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("press any key and enter to start stop watch");
+        String start =sc.next();
+        long startTime = System.currentTimeMillis();
+        System.out.println("press any key and enter to stop the timer");
+        String end =sc.next();
+        long stopTime =System.currentTimeMillis();
+        long elapseTime=stopTime-startTime;
+        System.out.println("Elapsed Time in millisecond is :"+elapseTime);
+    }
 }
