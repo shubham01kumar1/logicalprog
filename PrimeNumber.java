003ff2ccf92b5f564b37b6758c0d5a6023899f59
shubham01kumar1 <shubham01kumar1@gmail.com>
2021-08-21 00:08:51 +0530

    program to check for prime number

--- PrimeNumber.java
+++ PrimeNumber.java
@@ -1,4 +1,26 @@
 package logicalprog;
-
+import java.util.Scanner;
+//Program to check whether a number is a Prime number
 public class PrimeNumber {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter a Number");
+        int number = sc.nextInt();
+        PrimeNo(number);
+    }
+    static void PrimeNo(int number) {
+        boolean flag = false;
+        for (int i = 2; i <= number/2; i++) {
+            int prime = number % i;
+            if (prime == 0) {
+                flag = true;
+                break;
+            }
+        }
+        if(flag){
+            System.out.println("It is not a prime number");
+        }
+        else
+            System.out.println("It is a Prime Number ");
+    }
 }
